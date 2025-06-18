92d2511698f43588dc7eff41249d40d124f023d3
JiuRanYa <1084350607@qq.com>
2025-06-18 11:08:35 +0800

    feat: update logo SVG and adjust logo size in main navigation

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,7 +15,7 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6" />
+        <Icons.logo className="h-8 w-8" />
         <span className="inline-block font-bold">{t('name')}</span>
       </Link>
       {items?.length ? (
