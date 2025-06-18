ed08eaca5a32aa58415917d72c82221726c23628
JiuRanYa <1084350607@qq.com>
2025-06-18 16:45:43 +0800

    feat: update 2048 game configuration by removing old game entries and adding new image format

--- config/games.ts
+++ config/games.ts
@@ -8,24 +8,10 @@ export interface Game {
 
 export const games: Game[] = [
   {
-    id: 'snake',
-    title: 'Snake Game',
-    description: 'Classic snake game with modern design',
-    image: '/images/games/snake.png',
-    link: '/games/snake'
-  },
-  {
-    id: 'tetris',
-    title: 'Tetris',
-    description: 'Classic tetris game with modern design',
-    image: '/images/games/tetris.png',
-    link: '/games/tetris'
-  },
-  {
     id: '2048',
     title: '2048',
     description: '一个简单而有趣的数字益智游戏，通过滑动合并相同的数字，尝试得到2048！',
-    image: '/images/games/2048.png',
+    image: '/images/games/2048.jpg',
     link: '/games/2048'
   }
 ] 
