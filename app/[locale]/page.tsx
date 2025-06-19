2de8e789f9ee5b912d0d355e32d864481bd123e2
JiuRanYa <1084350607@qq.com>
2025-06-19 12:15:58 +0800

    feat: update IndexPage layout and localization for improved user experience and added 'Play Now' functionality

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -11,31 +11,33 @@ export default async function IndexPage() {
 
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          {t('title')}
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          {t('description')}
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          {t('documentation')}
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: 'outline' })}
-        >
-          {t('github')}
-        </Link>
+
+      <div className="flex flex-col gap-4 py-12 my-12">
+        <div className="flex max-w-[980px] flex-col items-start gap-2">
+          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+            {t('title')}
+          </h1>
+          <p className="max-w-[700px] text-lg text-muted-foreground">
+            {t('description')}
+          </p>
+        </div>
+        <div className="flex gap-4">
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={siteConfig.links.github}
+            className={buttonVariants({ variant: 'outline' })}
+          >
+            {t('github')}
+          </Link>
+          <Link
+            href={'/games/2048'}
+            rel="noreferrer"
+            className={buttonVariants()}
+          >
+            {t('palynow')}
+          </Link>
+        </div>
       </div>
 
       <div className="mt-12">
